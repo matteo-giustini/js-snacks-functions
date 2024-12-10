dc0a5b9e859883d1a2179d686ce70f93bdea73fa
matteo-giustini <matteo.giustini3@gmail.com>
2024-12-10 18:52:19 +0100

    feat: add "conta vocali"

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const numberOfWowels = (word) => {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let vowelsCount = 0
+    for(i = 0; i < word.length; i++){
+    const currentLetter = word[i]
+    if (vowels.includes(currentLetter)){
+        vowelsCount++
+    }
+}return vowelsCount
+
+
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(numberOfWowels(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
